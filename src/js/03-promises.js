1c55a46b0f90beb93bf849dd9a2122e0ada701a1
Maxym <103531640+MfD0@users.noreply.github.com>
2023-11-03 15:05:58 +0200

    3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from "notiflix";
+
+document.querySelector('.form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const delay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay + step * (position - 1))
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
